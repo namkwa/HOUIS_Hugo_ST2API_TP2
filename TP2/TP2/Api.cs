cecd59a940c0b1fb3690a9c05a014eaafd8eb040
namkwa <h.houis1507@gmail.com>
2022-06-18 22:25:47 +0200

    first commit

---
+++ TP2/TP2/Api.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TP2 {
+    public class Api {
+        public static async Task<CurrentWeather> GetDataFromApi(string location) {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=metric&appid=43db2aeef2a8280cb1c107389901a6b2")
+            };
+            using (var response = await client.SendAsync(request)) {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                CurrentWeather CurrentWeather = JsonSerializer.Deserialize<CurrentWeather>(body);
+                return CurrentWeather;
+            }
+        }
+    }
+}
---
+++ TP2/TP2/DateUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2 {
+    public class DateUtils {
+        public static DateTime ConvertDate(double date) {
+            DateTime convertedDate = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            convertedDate = convertedDate.AddSeconds(date).ToLocalTime();
+            return convertedDate;
+        }
+    }
+}
